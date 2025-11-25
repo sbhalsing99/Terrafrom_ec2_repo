d1e0deddea810d8ff96d77651bbcb889ff5af4f5
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-26 01:57:56 +0530

    Update security group ID for EC2 instance

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2_instance" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default_subnets.ids[0]
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.ec2_sg1.id]
   associate_public_ip_address = true
 
   tags = {
