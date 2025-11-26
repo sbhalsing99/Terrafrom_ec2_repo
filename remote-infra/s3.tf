08f5115cc5493c5e51f261e85f6b683c16ffd207
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-27 00:01:47 +0530

    modify 8

--- remote-infra/s3.tf
+++ remote-infra/s3.tf
@@ -1,24 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "shruti-terraform-state-bucket"   # MUST be globally unique
-  force_destroy = false
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_encryption" "encryption" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
+  bucket = "shruti-tf-backend-bucket"
 }
