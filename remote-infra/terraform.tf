5fa8cb0456d4dfd7bad7a605db35dd62bc9d94a7
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-26 23:46:11 +0530

    modify 5

--- remote-infra/terraform.tf
+++ remote-infra/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.4.0"
 
   backend "s3" {
-    bucket         = "shruti-terraform-state-bucket"
-    key            = "ec2/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
+    bucket        = "shruti-terraform-state-bucket"
+    key           = "ec2/terraform.tfstate"
+    region        = "us-east-1"
+    use_lockfile  = true
+    encrypt       = true
   }
 }
 
