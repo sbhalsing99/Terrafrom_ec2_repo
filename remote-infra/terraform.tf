88645aa35fc5d61f7a1b9469af66f84485fff9f2
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-26 23:13:46 +0530

    add backup file

---
+++ remote-infra/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "shruti-terraform-state-bucket"   # S3 bucket name
+    key            = "ec2/terraform.tfstate"           # path inside bucket
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
