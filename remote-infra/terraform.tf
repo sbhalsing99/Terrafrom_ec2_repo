4d218438fdcf0a603351c1b72cec0030b657a083
sbhalsing99 <bhalsing0899@gmail.com>
2025-11-26 23:35:54 +0530

    modify 2

--- remote-infra/terraform.tf
+++ remote-infra/terraform.tf
@@ -1,9 +1,7 @@
-terraform {
-  backend "s3" {
+backend "s3" {
     bucket         = "shruti-terraform-state-bucket"   # S3 bucket name
     key            = "ec2/terraform.tfstate"           # path inside bucket
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
-}
